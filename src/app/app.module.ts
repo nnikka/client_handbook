e3c3c3664d8aa629d67199b1d3125de248a767fb
nnikka <nikakhurashvili@gmail.com>
2019-04-18 10:56:09 +0400

    setup primerng module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimengModule } from './primeng/primeng.module'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
